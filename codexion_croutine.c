b810c751c7853815b995957c9d78e6f9adbdb216
Aymane El Halkouj <aymel-ha@e2r5p7.1337.ma>
2026-03-30 18:17:02 +0100

    progres... :)

--- codexion_croutine.c
+++ codexion_croutine.c
@@ -6,7 +6,7 @@
 /*   By: aymel-ha <aymel-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 14:23:02 by aymel-ha          #+#    #+#             */
-/*   Updated: 2026/03/30 16:00:20 by aymel-ha         ###   ########.fr       */
+/*   Updated: 2026/03/30 18:16:03 by aymel-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,62 @@ long choose_priority(t_coder *coder)
     return coder->last_compile_start + coder->sim->parse->time_to_burnout;
 }
 
-void dongle(t_codexion *codex, t_coder *coder, int dongle_pos)
+void take_dongle(t_codexion *codex, t_coder *coder, int dongle_pos)
 {
-    pthread_mutex_lock(&codex->dongles[dongle_pos].dongle_mutex);
-    if(!codex->dongles[dongle_pos].in_use && get_time_ms() >= codex->dongles[dongle_pos].available_at && !codex->dongles[dongle_pos].queue.size)
+    t_dongle *usb = &codex->dongles[dongle_pos];
+    struct timespec t;
+    long timestamp;
+    pthread_mutex_lock(&usb->dongle_mutex);
+    push_coder(coder->id, choose_priority(coder), &usb->queue);
+    while (usb->in_use || usb->queue.waiters[0].id != coder->id || get_time_ms() < usb->available_at)
     {
-        codex->dongles[dongle_pos].in_use = 1;
-        pthread_mutex_unlock(&codex->dongles[dongle_pos].dongle_mutex);
-        coder_logs(codex, get_time_ms(), coder->id, "has taken a dongle");
-        return ;
+        if (!usb->in_use && usb->queue.waiters[0].id == coder->id && get_time_ms() < usb->available_at)
+        {
+            t.tv_sec = usb->available_at / 1000;
+            t.tv_nsec = (usb->available_at % 1000) * 1000000;
+            pthread_cond_timedwait(&usb->dongle_cond, &usb->dongle_mutex, &t);
+        }
+        else
+            pthread_cond_wait(&usb->dongle_cond, &usb->dongle_mutex);
     }
-    push_coder(coder->id, choose_priority(coder), &codex->dongles[dongle_pos].queue);
-    
+    timestamp = get_time_ms();
+    remove_coder(&usb->queue);
+    usb->in_use = 1;
+    pthread_mutex_unlock(&usb->dongle_mutex);
+    coder_logs(codex, timestamp, coder->id, "has taken a dongle");
 }
 
+void take_two_dongles(t_codexion *codex, t_coder *coder, int rd, int ld)
+{
+    if (ld < rd)
+    {
+        take_dongle(codex, coder, ld);
+        take_dongle(codex, coder, rd);
+    }
+    else
+    {
+        take_dongle(codex, coder, rd);
+        take_dongle(codex, coder, ld);
+    }
+}
+void put_dongle(t_codexion *codex, int dongle_pos)
+{
+    t_dongle *usb = &codex->dongles[dongle_pos];
+    pthread_mutex_lock(&usb->dongle_mutex);
+    usb->in_use = 0;
+    usb->available_at = get_time_ms() + codex->parse->dongle_cooldown;
+    pthread_mutex_unlock(&usb->dongle_mutex);
+    pthread_cond_broadcast(&usb->dongle_cond);
+}
 void *coders_routine(void *arg)
 {
     t_coder *coder = (t_coder *)arg;
-    
+    int ld, rd;
+    ld = coder->id - 1;
+    rd = coder->id % coder->sim->parse->number_of_coders;
     while(!coder->sim->sim_over)
     {
-        
+        take_two_dongles(coder->sim, coder, rd, ld);
     }
 
     
